03afdee212fdeb038f6abd073641875e1219795e
Angiee Quispe <angieequispeg@gmail.com>
2022-09-29 13:46:38 -0300

    Agregando Archivos

---
+++ QuispeGuarachi_Angiee_JS_CambiarVariable.js
@@ -0,0 +1,10 @@
+var minimoAltura = 1;
+// Declaro como el minimo 1 en metros. En este caso es un dato Numerico
+var minimoEdad = 10;
+// Declaro como el minimo 10 para la edad. Dato de tipo Numerico
+var edad ,altura;
+// Quedan Undefined por que todavia no toman un valor
+//Para verificar la edad podriamos usar un boleano o una comparacion
+console.log(minimoAltura);
+console.log(minimoEdad);
+//Se imprimen los valores asignados como minimos para subir a la atracción 
