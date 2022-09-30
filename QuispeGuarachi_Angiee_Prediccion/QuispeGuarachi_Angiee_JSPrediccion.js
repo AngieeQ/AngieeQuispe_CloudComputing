7500fb42c8c45097f2ae32dea21bd76e561baeb3
AngieeQ <114453177+AngieeQ@users.noreply.github.com>
2022-09-30 12:32:23 -0300

    Add files via upload

---
+++ QuispeGuarachi_Angiee_Prediccion/QuispeGuarachi_Angiee_JSPrediccion.js
@@ -0,0 +1,39 @@
+    //PREDICCIO 1
+    function myBirthYearFunc()
+    {
+        console.log("Nací en" + 1980);
+    }//Va a indicar solamente el texto "Naci en " 
+    //Si es que se llama a la funcion ya que de momento no se la llama, solo se creó la funcion.
+    
+
+    //PREDICCION 2
+    birthYearInput = 1980
+    function myBirthYearFunc(birthYearInput)
+    {
+        console.log("Nací en " + birthYearInput);
+    }//El console.log indicara el mensaje "Naci en 1980", si es llamada correctamente, como asi:
+    //var r = myBirthYearFunc(birthYearInput);
+    //console.log(r);
+
+
+    //PREDICCION 3
+    function add(num1, num2)
+    {
+        console.log("¡Sumando números!");
+        console.log("num1 es: " + num1);
+        console.log("num2 es: " + num2);
+        var sum = num1 + num2;
+        console.log(sum);
+    }
+    //Si se llama a la funcion de esta forma 
+    //var r = add(num1,num2);
+    //console.log(r);
+    //el mensaje del console.log sera el siguiente:
+             // "¡Sumando numero!"   
+            //"num1 es: 10"  
+           // "num2 es: 20"   
+          //"30"
+    
+        
+             
+    
