9cd06b4057836a521c3baec19814a48fd197591a
AngieeQ <114453177+AngieeQ@users.noreply.github.com>
2022-09-30 00:48:32 -0300

    Add files via upload

---
+++ QuispeGuarachi_Angiee_Parametros/QuispeGuarachi_Angiee_FuncionYParametros.js
@@ -0,0 +1,24 @@
+var nombre = "Angiee";
+var hora = "15hs";
+var temnp = false;
+function CountDooku(temp)
+{
+    
+    if (nombre = "Angiee")
+    {
+        console.log("Hola ", nombre);  
+    }
+    if (temp == true)
+    {
+    console.log(" \n¡Que tengas buen dia! Son las ", hora);
+    }
+    else
+    {
+        console.log(" \n¡Ya se termina el dia! Son las ", hora);
+    }
+
+    console.log("\n Antes de que se me olvide ¡Voy por ti, Dooku!");
+}
+
+var result = CountDooku(nombre, hora);
+console.log(result);
